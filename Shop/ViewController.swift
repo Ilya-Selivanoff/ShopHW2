ad5042fd5acc1cdda8e1c824ce1cb2a528ee01e2
ilya Selivanov <selivan.1993@bk.ru>
2020-12-18 00:39:23 +0400

    add button to storyboard

--- Shop/ViewController.swift
+++ Shop/ViewController.swift
@@ -6,14 +6,24 @@
 //
 
 import UIKit
+import RealmSwift
+import Alamofire
+import SwiftyJSON
+
+
 
 class ViewController: UIViewController {
-
+               // Buttons
+    
+    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var registerButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+        
+        signInButton.layer.cornerRadius = 15
+        registerButton.layer.cornerRadius = 15
+        
+   }
 }
-
